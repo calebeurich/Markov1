531a52eac71a069967b64865e5264c277039472a
Caleb <calebeurich27@gmail.com>
2021-02-28 22:35:18 -0500

    comments
    commented code and changed variables

--- markov.py
+++ markov.py
@@ -2,16 +2,21 @@ import turtle
 import random
 import numpy as np
 
-ITERATIONS = 200
+#global variables
+ITERATIONS = 1000
+MIN_LINE_LENGTH = 5
+MAX_LINE_LENGTH = 10
 
-
+#define turtle variables
 s = turtle.getscreen()
 t = turtle.Turtle()
-turtle.screensize(canvwidth=500, canvheight=500) 
+turtle.screensize(canvwidth=500, canvheight=500)
+t.speed(10)
 
-initial_direction = [.25, .25, .25, .25]
+
+initial_direction = [.25, .25, .25, .25] #initial direction probability
 directions = [90, 0, 270, 180]
-direction_keys = {90:0, 0:1, 270:2, 180:3}
+direction_keys = {90:0, 0:1, 270:2, 180:3} #dictionary to associate direction with index in transition matrix
 #direction matrix where the order is N,E,S,W
 #.5 chance to go straight, .25 chance to change 90 degrees
 direction_matrix = [
@@ -21,9 +26,10 @@ direction_matrix = [
     [.25, 0, .25, .5]
     ]
 
-initial_color = [1, 0, 0, 0, 0, 0]
-color_keys = {"red":0, "orange":1, "yellow":2, "green":3, "blue":4, "purple":5}
+initial_color = [1, 0, 0, 0, 0, 0] #initial color probability
+color_keys = {"red":0, "orange":1, "yellow":2, "green":3, "blue":4, "purple":5} #dictionary to associate color with index in transition matrix
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+#.5 chance to remain the same color, or advance one color in the rainbow
 color_matrix = [
     [.5, .5, 0, 0, 0, 0], 
     [0, .5, .5, 0, 0, 0], 
@@ -33,9 +39,10 @@ color_matrix = [
     [.5, 0, 0, 0, 0, .5],
     ]
 
-initial_size = [.25, .25, .25, .25]
-size_keys = {1:0, 2.5:1, 5:2, 10:3}
+initial_size = [.25, .25, .25, .25] #initial pensize probability
+size_keys = {1:0, 2.5:1, 5:2, 10:3} #dictionary to associate size with transition matrix
 sizes = [1, 2.5, 5, 10]
+#.5 chance to remain the same size, .5 chance to change size one order split between options
 size_matrix = [
     [.5, .5, 0, 0], 
     [.25, .5, .25, 0], 
@@ -43,37 +50,42 @@ size_matrix = [
     [0, 0, .5, .5]
     ]
 
+#helper function to pick a new attribute based off the markov property
 def pick_attribute(attributes, matrix):
     new_attribute = np.random.choice(attributes, p=matrix)
     return new_attribute
 
+#function that returns initial attributes based off of initial propabilities
 def markov_start():
     first_direction = np.random.choice(directions, p=initial_direction)
     first_color = np.random.choice(colors, p=initial_color)
     first_size = np.random.choice(sizes, p = initial_size)
     return [first_direction, first_color, first_size]
 
+#function that returns new attributes based off of previous attributes while following the markov property
 def markov_continue(previous_direction, previous_color, previous_size):
     new_direction = pick_attribute(directions, direction_matrix[direction_keys.get(previous_direction)])
     new_color = pick_attribute(colors, color_matrix[color_keys.get(previous_color)])
     new_size = pick_attribute(sizes, size_matrix[size_keys.get(previous_size)])
     return [new_direction, new_color, new_size]
 
+#helper function that moves the turtle object as specified by the current attributes
 def move(direction, color, size):
     t.setheading(direction)
     t.color(color)
     t.pensize(size)
-    t.forward(random.randrange(0, 10))
+    t.forward(random.randrange(MIN_LINE_LENGTH, MAX_LINE_LENGTH))
 
+#main function to initialize values then continue the markov chain for a set number of iterations
 def main():
     initial_values = markov_start()
     move(initial_values[0], initial_values[1], initial_values[2])
     for i in range(ITERATIONS):
-        if i == 0:
+        if i == 0: # feed in initial values if this is the first iteration
             previous_values = initial_values
         new_values = markov_continue(previous_values[0], previous_values[1], previous_values[2])
         move(new_values[0], new_values[1], new_values[2])
-        previous_values = new_values
+        previous_values = new_values #set the next set of previous values to the values that were just used
 
 if __name__ == "__main__": 
     main() 
